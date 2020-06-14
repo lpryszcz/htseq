7f908af49e7a2f16ba33740a26eeb68e59308e52
Fabio Zanini <fabio.zanini@fastmail.fm>
2020-06-14 14:59:24 +1000

    Test on Travis on the right folder

--- .travis_test.sh
+++ .travis_test.sh
@@ -17,27 +17,24 @@ fi
 
 echo "python: ${PYTHON}"
 
-py_fdn="python$(${PYTHON} --version 2>&1 | cut -f2 -d ' ' | cut -f1 -d'.')/"
-echo "py_fdn: ${py_fdn}"
-
 echo 'Running tests...'
 
 echo 'General tests...'
-${PYTHON} "${py_fdn}test/test_general.py"
+${PYTHON} test/test_general.py
 if [ $? != 0 ]; then
     exit 1
 fi
 echo 'done!'
 
 echo 'Doctests...'
-${PYTHON} "${py_fdn}test/test.py"
+${PYTHON} test/test.py
 if [ $? != 0 ]; then
     exit 1
 fi
 echo 'done!'
 
 echo 'htseq-count...'
-${PYTHON} "${py_fdn}test/test_htseq-count.py"
+${PYTHON} test/test_htseq-count.py
 if [ $? != 0 ]; then
     exit 1
 fi
