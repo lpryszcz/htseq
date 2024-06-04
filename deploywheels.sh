eb6178b6f4eee11cfa6af0c2f07821faf7ba8490
Fabio Zanini <fabio.zanini@fastmail.fm>
2024-06-04 12:37:33 +1000

    fix a few CD issues

--- deploywheels.sh
+++ deploywheels.sh
@@ -18,34 +18,34 @@ set -xeuo pipefail
 # only deploy builds for a release_<sematic-version>_RC?? tag to testpypi
 echo "Figure out if a release is appropriate for this tag: ${GITHUB_REF}"
 if [ -z $GITHUB_REF ]; then
-  echo 'No GITHUB_REF, exit'
-  exit 0
+	echo 'No GITHUB_REF, exit'
+	exit 0
 fi
 TAG=$(echo $GITHUB_REF | cut -f3 -d/)
 TAG1=$(echo $TAG | cut -f1 -d_)
 TAG2=$(echo $TAG | cut -f2 -d_)
 TAG3=$(echo $TAG | cut -f3 -d_)
 if [ -z $TAG2 ]; then
-  echo 'No TAG2, exit'
-  exit 0;
+	echo 'No TAG2, exit'
+	exit 0
 fi
 if [ $TAG1 != 'release' ] || [ $TAG2 != $(cat /io/VERSION) ]; then
-  echo 'No release tag or wrong version, exit'
-  exit 0;
+	echo 'No release tag or wrong version, exit'
+	exit 0
 fi
 
 # deploy onto pypitest unless you have no RC
 if [ -z $TAG3 ]; then
-  TWINE_PASSWORD=${TWINE_PASSWORD_PYPI}
-  TWINE_REPOSITORY='https://upload.pypi.org/legacy/'
-  echo 'Deploying to production pypi'
+	TWINE_PASSWORD=${TWINE_PASSWORD_PYPI}
+	TWINE_REPOSITORY='https://upload.pypi.org/legacy/'
+	echo 'Deploying to production pypi'
 elif [ ${TAG3:0:2} == 'RC' ]; then
-  TWINE_PASSWORD=${TWINE_PASSWORD_TESTPYPI}
-  TWINE_REPOSITORY='https://test.pypi.org/legacy/'
-  echo 'Deploying to testpypi'
+	TWINE_PASSWORD=${TWINE_PASSWORD_TESTPYPI}
+	TWINE_REPOSITORY='https://test.pypi.org/legacy/'
+	echo 'Deploying to testpypi'
 else
-  echo "Tag not recognized: $GITHUB_REF"
-  exit 1
+	echo "Tag not recognized: $GITHUB_REF"
+	exit 1
 fi
 
 # Deploy binary packages
@@ -62,26 +62,27 @@ echo "HTSEQ_WHEEL_FILE: ${HTSEQ_WHEEL_FILE}"
 ERRS=0
 
 for PYBIN in ${PYBINS}; do
-  ${PYBIN}/pip install twine
-  PYVER=$(basename $(dirname ${PYBIN}))
-  echo "PYVER=$PYVER"
-  echo "TWINE_REPOSITORY=$TWINE_REPOSITORY"
-  #echo "TWINE_USERNAME=$TWINE_USERNAME"
-  #echo "TWINE_PASSWORD=$TWINE_PASSWORD"
+	${PYBIN}/pip install twine
+	PYVER=$(basename $(dirname ${PYBIN}))
+	echo "PYVER=$PYVER"
+	echo "TWINE_REPOSITORY=$TWINE_REPOSITORY"
+	#echo "TWINE_USERNAME=$TWINE_USERNAME"
+	#echo "TWINE_PASSWORD=$TWINE_PASSWORD"
 
-  if [ x$SOURCE_VERSION == x$PYTHON_VERSION ]; then
-    echo "Deploy source code"
-    ${PYBIN}/twine upload --repository-url "${TWINE_REPOSITORY}" -u "${TWINE_USERNAME}" -p "${TWINE_PASSWORD}" /io/wheelhouse/HTSeq-${HTSEQ_VERSION}.tar.gz
-    if [ $? != 0 ]; then
-      ERRS=1
-    fi
-  fi
+	if [ x$SOURCE_VERSION == x$PYTHON_VERSION ]; then
+		echo "Deploy source code"
+		# NOTE: setuptools now automatically uses lowercase for package files...
+		${PYBIN}/twine upload --repository-url "${TWINE_REPOSITORY}" -u "${TWINE_USERNAME}" -p "${TWINE_PASSWORD}" /io/wheelhouse/htseq-${HTSEQ_VERSION}.tar.gz
+		if [ $? != 0 ]; then
+			ERRS=1
+		fi
+	fi
 
-  echo "Deploy binary wheel"
-  ${PYBIN}/twine upload --repository-url "${TWINE_REPOSITORY}" -u "${TWINE_USERNAME}" -p "${TWINE_PASSWORD}" ${HTSEQ_WHEEL_FILE}
-  if [ $? != 0 ]; then
-    ERRS=1
-  fi
+	echo "Deploy binary wheel"
+	${PYBIN}/twine upload --repository-url "${TWINE_REPOSITORY}" -u "${TWINE_USERNAME}" -p "${TWINE_PASSWORD}" ${HTSEQ_WHEEL_FILE}
+	if [ $? != 0 ]; then
+		ERRS=1
+	fi
 done
 
 exit $ERRS
